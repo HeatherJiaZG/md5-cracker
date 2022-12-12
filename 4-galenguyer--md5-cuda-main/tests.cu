37582c6541581d777b96af740f871a64b5743da8
yjia42 <yingxin@DESKTOP-3ADOK10.localdomain>
2022-12-12 08:50:08 -0600

    add file

--- 4-galenguyer--md5-cuda-main/tests.cu
+++ 4-galenguyer--md5-cuda-main/tests.cu
@@ -3,7 +3,45 @@
 #include <string.h>
 #include "md5.cu"
 
-char* hash(const char* h_str) {
+struct deviceInfo{
+	struct cudaDeviceProp prop; // Device Properties
+	int id; // Device ID
+	int max_threads; // Device max threads per block
+	int max_blocks; // Device max blocks
+	int global_memory_len;
+};
+
+
+#define REQUIRED_SHARED_MEMORY 64
+#define FUNCTION_PARAM_ALLOC 256
+struct deviceInfo device; 
+
+void getOptimalThreads(struct deviceInfo * device) {
+	int max_threads;
+	int max_blocks;
+	int shared_memory;
+
+	max_threads = device->prop.maxThreadsPerBlock;
+	shared_memory = device->prop.sharedMemPerBlock - FUNCTION_PARAM_ALLOC;
+	
+	// calculate the most threads that we can support optimally
+	
+	while ((shared_memory / max_threads) < REQUIRED_SHARED_MEMORY) { max_threads--; } 
+
+	// now we spread our threads across blocks 
+	
+	max_blocks = 40;		
+
+	device->max_threads = max_threads;		// most threads we support
+	device->max_blocks = max_blocks;		// most blocks we support
+
+	// now we need to have (device.max_threads * device.max_blocks) number of words in memory for the graphics card
+	
+	device->global_memory_len = (device->max_threads * device->max_blocks) * 64;
+}
+
+
+char* md5_hash(const char* h_str) {
     char* d_str;
     unsigned char* h_res = (unsigned char*)malloc(sizeof(unsigned char)*(32 + 1));
     unsigned char* d_res;
@@ -11,7 +49,7 @@ char* hash(const char* h_str) {
     cudaMalloc((void**)&d_res, sizeof(char) * 32);
     cudaMemcpy(d_str, h_str, sizeof(char) * strlen(h_str), cudaMemcpyHostToDevice);
 
-    md5<<<1, 1>>>(d_str, (uint32_t)strlen(h_str), d_res);
+    md5<<<device->max_blocks, device->max_threads>>>(d_str, (uint32_t)strlen(h_str), d_res);
 
     cudaMemcpy(h_res, d_res, sizeof(unsigned char)*(32), cudaMemcpyDeviceToHost);
 
@@ -37,11 +75,17 @@ int run_test(const char* name, const char* result, const char* expected) {
 
 
 int main() {
+
+	// Set device id to 0 (Use fastest device)
+	device.id = 0;
+	cudaGetDeviceProperties(&device.prop, device.id);
+    getOptimalThreads(&device);
+
     int passed = 0, failed = 0;
 
     // run_test("md5(\"\")", hash(""), "d41d8cd98f00b204e9800998ecf8427e") ? passed++ : failed++;
     // run_test("md5(\"a\")", hash("a"), "0cc175b9c0f1b6a831c399e269772661") ? passed++ : failed++;
-    run_test("md5(\"abc\")", hash("abc"), "900150983cd24fb0d6963f7d28e17f72") ? passed++ : failed++;
+    run_test("md5(\"abc\")", md5_hash("abc"), "900150983cd24fb0d6963f7d28e17f72") ? passed++ : failed++;
     // run_test("md5(\"message digest\")", hash("message digest"), "f96b697d7cb7938d525a2f31aaf161d0") ? passed++ : failed++;
     // run_test("md5(\"abcdefghijklmnopqrstuvwxyz\")", \
     //     hash("abcdefghijklmnopqrstuvwxyz"), \
