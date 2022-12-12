eec689e0236daec27c086e6cd00898d62beb2e69
yjia42 <yingxin@DESKTOP-3ADOK10.localdomain>
2022-12-12 02:02:33 -0600

    add file

--- 4-galenguyer--md5-cuda-main/tests.cu
+++ 4-galenguyer--md5-cuda-main/tests.cu
@@ -3,6 +3,19 @@
 #include <string.h>
 #include "md5.cu"
 
+using namespace std;
+ 
+
+std::string getCurrentPwd(int i, char chars[], int len, int n)
+{
+    std::string result = "";
+    for (int j = 0; j < n; j++) {
+        result += chars[i % len];
+        i /= len;
+    }
+    return result;
+}
+
 char* hash(const char* h_str) {
     char* d_str;
     unsigned char* h_res = (unsigned char*)malloc(sizeof(unsigned char)*(32 + 1));
@@ -37,12 +50,13 @@ int run_test(const char* name, const char* result, const char* expected) {
 
 
 int main() {
-    int passed = 0, failed = 0;
+    // int passed = 0, failed = 0;
 
     // run_test("md5(\"\")", hash(""), "d41d8cd98f00b204e9800998ecf8427e") ? passed++ : failed++;
     // run_test("md5(\"a\")", hash("a"), "0cc175b9c0f1b6a831c399e269772661") ? passed++ : failed++;
-    run_test("md5(\"abc\")", hash("abc"), "900150983cd24fb0d6963f7d28e17f72") ? passed++ : failed++;
-    run_test("md5(\"abc\")", hash("bad"), "bae60998ffe4923b131e3d6e4c19993e") ? passed++ : failed++;
+    // run_test("md5(\"abc\")", hash("abc"), "900150983cd24fb0d6963f7d28e17f72") ? passed++ : failed++;
+
+    // run_test("md5(\"bad\")", hash("bad"), "bae60998ffe4923b131e3d6e4c19993e") ? passed++ : failed++;
     // run_test("md5(\"message digest\")", hash("message digest"), "f96b697d7cb7938d525a2f31aaf161d0") ? passed++ : failed++;
     // run_test("md5(\"abcdefghijklmnopqrstuvwxyz\")", \
     //     hash("abcdefghijklmnopqrstuvwxyz"), \
@@ -54,7 +68,28 @@ int main() {
     //     hash("12345678901234567890123456789012345678901234567890123456789012345678901234567890"), \
     //     "57edf4a22be3c955ac49da2e2107b67a") ? passed++ : failed++;
 
-    printf("Tests Passed: %i\n", passed);
-    printf("Tests Failed: %i\n", failed);
-    return failed;
+    // printf("Tests Passed: %i\n", passed);
+    // printf("Tests Failed: %i\n", failed);
+    // return failed;
+
+
+    // int n = stoi(argv[1]); // password length
+    int n=3;
+    // std::string target = argv[2]; // target hash
+    std::string target = "bae60998ffe4923b131e3d6e4c19993e";
+    std::string result = "";
+
+    cout << "target = " << target << endl;
+
+    char chars[] = { 'a', 'b', 'd', '3', '4', '5', '6', '7', '8', '9' };
+    int len = sizeof(chars) / sizeof(chars[0]);
+    for (int i = 0; i < (int)pow(len, n); i++) {
+        std::string current_pwd = getCurrentPwd(i, chars, len, n);
+        if(strcmp(target, hash(current_pwd)) == 0) {
+            result = current_pwd;
+            break;
+        }
+    }
+    cout << "password is: " << result << endl;
+
 }
