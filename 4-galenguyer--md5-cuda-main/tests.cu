5dd192b76757b3a9cfecf266a0ac2e38f8942453
yjia42 <yingxin@DESKTOP-3ADOK10.localdomain>
2022-12-12 11:51:39 -0600

    add file

--- 4-galenguyer--md5-cuda-main/tests.cu
+++ 4-galenguyer--md5-cuda-main/tests.cu
@@ -76,33 +76,22 @@ int run_test(const char* name, const char* result, const char* expected) {
     }
 }
 
-char *target = "07159c47ee1b19ae4fb9c40d480856c4";
+char *target = "de8be12caf23444b451ea27be98dc8a9";
 
 void print_str(const char str[],std::string prefix,const int n, const int lenght) {
-        if (lenght == 1)
-            {
-                for (int j = 0; j < n; j++) {
-                    if (strcmp(target, md5_hash((prefix + str[j])).c_str()) == 0) {
-                        std::cout << "Cracked = " << prefix + str[j] << std::endl;
-                        break;
-                    }
+    if (lenght == 1) {
+            for (int j = 0; j < n; j++) {
+                if (strcmp(target, md5_hash((prefix + str[j]).c_str())) == 0) {
+                    std::cout << "Cracked = " << prefix + str[j] << std::endl;
+                    break;
                 }
-                // std::cout << prefix + str[j] << std::endl;
-
-            }//Base case: lenght = 1, print the string "lenght" times + the remaining letter
-
-        else
-            {
-               // One by one add all characters from "str" and recursively call for "lenght" equals to "lenght"-1
-                for (int i = 0; i < n; i++)
-
-                // Next character of input added
-                print_str(str, prefix + str[i], n, lenght - 1);
-                // "lenght" is decreased, because we have added a new character
-
             }
-
-    }
+        }
+    else {
+            for (int i = 0; i < n; i++)
+                print_str(str, prefix + str[i], n, lenght - 1);
+        }
+}
 
 
 int main() {
@@ -116,8 +105,10 @@ int main() {
 
     // run_test("md5(\"\")", hash(""), "d41d8cd98f00b204e9800998ecf8427e") ? passed++ : failed++;
     // run_test("md5(\"a\")", hash("a"), "0cc175b9c0f1b6a831c399e269772661") ? passed++ : failed++;
-    run_test("md5(\"abc\")", md5_hash("abc"), "900150983cd24fb0d6963f7d28e17f72") ? passed++ : failed++;
-    run_test("md5(\"ba\")", md5_hash("ba"), "07159c47ee1b19ae4fb9c40d480856c4") ? passed++ : failed++;
+    // run_test("md5(\"abc\")", md5_hash("abc"), "900150983cd24fb0d6963f7d28e17f72") ? passed++ : failed++;
+
+    // run_test("md5(\"ba\")", md5_hash("1998012"), "de8be12caf23444b451ea27be98dc8a9") ? passed++ : failed++;
+    
     // run_test("md5(\"message digest\")", hash("message digest"), "f96b697d7cb7938d525a2f31aaf161d0") ? passed++ : failed++;
     // run_test("md5(\"abcdefghijklmnopqrstuvwxyz\")", \
     //     hash("abcdefghijklmnopqrstuvwxyz"), \
@@ -135,7 +126,7 @@ int main() {
 
 
     int len = 2;
-    char str[] = {'a', 'b', 'c', 'd'};
+    char str[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     int n = sizeof(str);
     print_str(str, "", n, len);
 
