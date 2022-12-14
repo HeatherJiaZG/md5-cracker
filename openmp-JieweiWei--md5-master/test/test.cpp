a2566d8a1871ccb7760bde2fb6b4be856b8e5364
yjia42 <yingxin@DESKTOP-3ADOK10.localdomain>
2022-12-14 07:01:07 -0600

    add file

--- openmp-JieweiWei--md5-master/test/test.cpp
+++ openmp-JieweiWei--md5-master/test/test.cpp
@@ -10,6 +10,8 @@
 
 #include <iostream>
 #include "../src/md5.h"
+#include <string>
+#include <bits/stdc++.h>
 
 using std::cout;
 using std::endl;
@@ -20,11 +22,21 @@ void printMD5(const string& message) {
 }
 
 int main() {
-  printMD5("");
-  printMD5("a");
-  printMD5("abc");
-  printMD5("message digest");
-  printMD5("abcdefghijklmnopqrstuvwxyz");
-  printMD5("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+  int n = stoi(argv[1]); // password length
+  std::string target = argv[2]; // target hash
+  std::string result = "";
+
+  cout << "target = " << target << endl;
+
+  char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+  int len = sizeof(chars) / sizeof(chars[0]);
+  for (int i = 0; i < (int)pow(len, n); i++) {
+      std::string current_pwd = getCurrentPwd(i, chars, len, n);
+      if (target == MD5(current_pwd).toStr()) {
+          result = current_pwd;
+          break;
+      }
+  }
+  cout << "password is: " << result << endl;
 	return 0;
 }
