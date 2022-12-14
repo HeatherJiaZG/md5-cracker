af73ce295ae0b50cf399d2595cfe0092f84eca93
yjia42 <yingxin@DESKTOP-3ADOK10.localdomain>
2022-12-14 07:14:50 -0600

    add file

--- openmp-JieweiWei--md5-master/test/test.cpp
+++ openmp-JieweiWei--md5-master/test/test.cpp
@@ -12,9 +12,13 @@
 #include "../src/md5.h"
 #include <string>
 #include <bits/stdc++.h>
+#include <chrono>
 
 using std::cout;
 using std::endl;
+
+using std::chrono::duration;
+using std::chrono::high_resolution_clock;
 
 void printMD5(const string& message) {
   cout << "md5(\"" << message << "\") = "
@@ -37,8 +41,14 @@ int main(int argc, char *argv[]) {
 
   cout << "target = " << target << endl;
 
+  high_resolution_clock::time_point start;
+  high_resolution_clock::time_point end;
+  duration<double, std::milli> duration_sec;
+  start = high_resolution_clock::now();
+
   char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
   int len = sizeof(chars) / sizeof(chars[0]);
+  
   for (int i = 0; i < (int)pow(len, n); i++) {
       std::string current_pwd = getCurrentPwd(i, chars, len, n);
       if (target == MD5(current_pwd).toStr()) {
@@ -46,6 +56,12 @@ int main(int argc, char *argv[]) {
           break;
       }
   }
+
+  end = high_resolution_clock::now();
+  duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+
   cout << "password is: " << result << endl;
+  cout << duration_sec.count() << endl;
+
 	return 0;
 }
