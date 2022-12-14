8599c9d475af6fbb0796db978f5bb8189cf3dbf3
yjia42 <yingxin@DESKTOP-3ADOK10.localdomain>
2022-12-14 07:28:12 -0600

    add file

--- openmp-JieweiWei--md5-master/temp_test.cpp
+++ openmp-JieweiWei--md5-master/temp_test.cpp
@@ -20,9 +20,6 @@ using std::endl;
 using std::chrono::duration;
 using std::chrono::high_resolution_clock;
 
-char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-
 void printMD5(const string& message) {
   cout << "md5(\"" << message << "\") = "
        << MD5(message).toStr() << endl;
@@ -37,23 +34,9 @@ std::string getCurrentPwd(int i, char chars[], int len, int n)
     }
     return result;
 }
-
-std::string crack(int len, int n, std::string target) {
-    std::string result = "";
-    for (int i = 0; i < (int)pow(len, n); i++) {
-        std::string current_pwd = getCurrentPwd(i, chars, len, n);
-        if (target == MD5(current_pwd).toStr()) {
-            result = current_pwd;
-            break;
-        }
-    }
-    return result;
-}
-
 int main(int argc, char *argv[]) {
   int n = std::stoi(argv[1]); // password length
   std::string target = argv[2]; // target hash
-  int len = sizeof(chars) / sizeof(chars[0]);
   std::string result = "";
 
   cout << "target = " << target << endl;
@@ -62,22 +45,23 @@ int main(int argc, char *argv[]) {
   high_resolution_clock::time_point end;
   duration<double, std::milli> duration_sec;
   start = high_resolution_clock::now();
-  
-//   for (int i = 0; i < (int)pow(len, n); i++) {
-//       std::string current_pwd = getCurrentPwd(i, chars, len, n);
-//       if (target == MD5(current_pwd).toStr()) {
-//           result = current_pwd;
-//           break;
-//       }
-//   }
 
-  result = crack(len, n, target);
+  char chars[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+  int len = sizeof(chars) / sizeof(chars[0]);
+  
+  for (int i = 0; i < (int)pow(len, n); i++) {
+      std::string current_pwd = getCurrentPwd(i, chars, len, n);
+      if (target == MD5(current_pwd).toStr()) {
+          result = current_pwd;
+          break;
+      }
+  }
 
   end = high_resolution_clock::now();
   duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
 
   cout << "password is: " << result << endl;
-  cout << "Time spent: " << duration_sec.count() << " ms" << endl;
+  cout << duration_sec.count() << endl;
 
 	return 0;
 }
